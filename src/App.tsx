13eca6f5336f47c9a32384b21bdf9ccea4be9492
Tu Huynh <huynhlethanhtu@gmail.com>
2024-10-17 09:45:58 +0700

    route setup completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
 import { Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home";
-import About from "./pages/About";
+import MainLayout from "./layouts/MainLayout";
+
+import About from "./pages/main/About";
+import Home from "./pages/main/Home";
+import Profile from "./pages/main/Profile";
+import Settings from "./pages/main/Settings";
+
+import Dashboard from "./pages/chat_app/Dashboard";
 
 import "./App.css";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
+      <Route path="" element={<MainLayout/>} >
+        <Route path="" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="settings" element={<Settings />} />
+      </Route>
+
+      <Route path="chat">
+        <Route path="" element={<Dashboard />} />
+      </Route>
     </Routes>
   );
 }
