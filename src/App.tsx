3ae8212da8a36c268304d66c9be199fa84cbd32d
Tu Huynh <huynhlethanhtu@gmail.com>
2024-11-22 16:49:04 +0700

    login form component

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,13 @@ import Home from "./pages/main/Home";
 import Profile from "./pages/main/Profile";
 import Settings from "./pages/main/Settings";
 import Dashboard from "./pages/main/Dashboard";
+
+import Login from "./pages/auth/Login";
+
 import Messages from "./pages/chat_app/Chat";
+
 import { ThemeProvider } from "@/components/ui-shadcn/theme-provider"
+
 import "./App.css";
 
 function App() {
@@ -19,6 +24,10 @@ function App() {
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="profile" element={<Profile />} />
           <Route path="settings" element={<Settings />} />
+        </Route>
+
+        <Route path="auth">
+          <Route path="login" element={<Login />}></Route>
         </Route>
 
         <Route path="chat">
