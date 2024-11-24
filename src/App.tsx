99bfade735adbbbf292d010074f179a6eae94967
Tu Huynh <huynhlethanhtu@gmail.com>
2024-11-25 01:50:50 +0700

    Protected route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate, Outlet } from "react-router-dom";
 
 import MainLayout from "./layouts/MainLayout";
 import About from "./pages/main/About";
@@ -13,30 +13,46 @@ import Register from "./pages/auth/Register";
 
 import Messages from "./pages/chat_app/Chat";
 
+import NotFound from "./pages/error/404";
+
 import { ThemeProvider } from "@/components/ui-shadcn/theme-provider";
 
 import "./App.css";
+
+const ProtectedRoute = (props: any) => {
+  // let auth={'token': false}
+  return props.isAuthenticated ? <Outlet /> : <Navigate to="/auth/login" />;
+};
+
+const isAuthenticated = false;
 
 function App() {
   return (
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       <Routes>
-        <Route path="" element={<MainLayout />}>
-          <Route path="" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="settings" element={<Settings />} />
+        {/* Main-application pages */}
+        <Route element={<ProtectedRoute isAuthenticated={isAuthenticated} />}>
+          <Route path="" element={<MainLayout />}>
+            <Route path="" element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="settings" element={<Settings />} />
+          </Route>
+
+          <Route path="chat">
+            <Route path="messages" element={<Messages />} />
+          </Route>
         </Route>
 
+        {/* Authentication pages */}
         <Route path="auth" element={<AuthLayout />}>
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
         </Route>
 
-        <Route path="chat">
-          <Route path="messages" element={<Messages />} />
-        </Route>
+        {/* 404 page */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </ThemeProvider>
   );
