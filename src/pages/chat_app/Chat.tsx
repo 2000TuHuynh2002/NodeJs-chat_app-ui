097db6147440d7194f993d16f1ee18cf2516775a
Tu Huynh <huynhlethanhtu@gmail.com>
2024-12-09 16:50:50 +0700

    add scroll area

--- src/pages/chat_app/Chat.tsx
+++ src/pages/chat_app/Chat.tsx
@@ -4,7 +4,7 @@ const Messages = () => {
   return (
     <>
       <div className="grid xl:grid-cols-12 grid-cols-1">
-        <div className="xl:col-span-3 pl-6 pt-6 pr-4 border-r col-span-1">
+        <div className="xl:col-span-3 pl-6 pt-6 pr-4 border-r col-span-1 h-[calc(100vh-6.6rem)]">
           <Conversations />
         </div>
         <div className="xl:col-span-9 hidden"></div>
